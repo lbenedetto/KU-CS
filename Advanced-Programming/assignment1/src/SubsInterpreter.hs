77a54dc5de0d62725b609950c16333a7a870f750
lbenedetto <larsbenedetto@gmail.com>
2018-09-15 19:37:47 +0200

    Began implementation

--- Advanced-Programming/assignment1/src/SubsInterpreter.hs
+++ Advanced-Programming/assignment1/src/SubsInterpreter.hs
@@ -35,21 +35,23 @@ type Context = (Env, PEnv)
 initialContext :: Context
 initialContext = (Map.empty, initialPEnv)
   where initialPEnv =
-          Map.fromList [ ("===", undefined)
-                       , ("<", undefined)
-                       , ("+", undefined)
-                       , ("*", undefined)
-                       , ("-", undefined)
-                       , ("%", undefined)
+          Map.fromList [ ("===", (===:))
+                       , ("<", (<:))
+                       , ("+", (+:))
+                       , ("*", (*:))
+                       , ("-", (-:))
+                       , ("%", (%:))
                        , ("Array", mkArray)
                        ]
 
 newtype SubsM a = SubsM {runSubsM :: Context -> Either Error (a, Env)}
 
 instance Monad SubsM where
-  return x = undefined 
-  m >>= f = undefined
-  fail s = undefined
+  return x = SubsM (\(e, p) -> Right (x, e))
+  SubsM m >>= f = SubsM $ (\c -> case m c of
+                             Right (a,e) -> runSubsM (f a) (e, snd c)
+                             Left l -> Left l )
+  fail s = SubsM (\_ -> Left s)
 
 -- You may modify these if you want, but it shouldn't be necessary
 instance Functor SubsM where
@@ -57,6 +59,44 @@ instance Functor SubsM where
 instance Applicative SubsM where
   pure = return
   (<*>) = ap
+
+--Bool To Value
+btv :: Bool -> Value
+btv v = if v then TrueVal else FalseVal
+
+-- Get the standard error message
+stderr :: String -> Either Error Value
+stderr s = Left $ s ++ "called with wrong number of type of arguments"
+
+(===:) :: Primitive
+(===:) [IntVal a, IntVal b] = Right $ btv $ a == b
+(===:) [UndefinedVal, UndefinedVal] = Right TrueVal
+(===:) [TrueVal, TrueVal] = Right TrueVal
+(===:) [FalseVal, FalseVal] = Right TrueVal
+(===:) [ArrayVal (x:xs), ArrayVal (y:ys)] =
+        do curr <- ((===:) [x,y])
+           rest <- ((===:) [ArrayVal xs, ArrayVal ys])
+           return $ btv $ (curr == TrueVal) && (rest == TrueVal)
+(===:) [ArrayVal [], ArrayVal []] = Right TrueVal
+(===:) [a,b] = Right FalseVal
+(===:) _ = stderr "==="
+
+(<:) :: Primitive
+(<:) [IntVal a, IntVal b] = Right $ btv (a < b)
+(<:) [StringVal a, StringVal b] = Right $ btv (a < b)
+(<:) _ = stderr "<"
+
+(+:) :: Primitive
+(+:) = undefined
+
+(*:) :: Primitive
+(*:) = undefined
+
+(-:) :: Primitive
+(-:) = undefined
+
+(%:) :: Primitive
+(%:) = undefined
 
 mkArray :: Primitive
 mkArray [IntVal n] | n >= 0 = return $ ArrayVal (replicate n UndefinedVal)
